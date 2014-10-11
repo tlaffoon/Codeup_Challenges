8fdd6aaab6e38f7fef36cf1c17a5001981a27078
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-10-11 13:55:01 -0500

    This is a complete solution.

--- alphabet_soup/alphabet_soup.php
+++ alphabet_soup/alphabet_soup.php
@@ -7,7 +7,7 @@ Problem:
  Create a function alphabet_soup($str) that accepts a string 
 and will return the string in alphabetical order. 
 
-E.g. "hello world" becomes "ehllo dlorw". 
+ E.g. "hello world" becomes "ehllo dlorw". 
 So make sure your function separates and alphabetizes each word separately.		
 
 */
