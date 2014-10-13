bbdaa98a725347066ba88126c3777711092ea35c
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-10-13 08:33:32 -0500

    Added additional comments; cleaned up for readability.

--- alphabet_soup/alphabet_soup.php
+++ alphabet_soup/alphabet_soup.php
@@ -5,7 +5,7 @@
 Problem:
 
  Create a function alphabet_soup($str) that accepts a string 
-and will return the string in alphabetical order. 
+and will return the string in alphabetical order.
 
  E.g. "hello world" becomes "ehllo dlorw". 
 So make sure your function separates and alphabetizes each word separately.		
@@ -20,20 +20,24 @@ function alphabet_soup($string) {
 
 	// With each word, create array of letters
 	foreach ($words as $word) {
+		
 		// Create a new array from each word with str_split()
 		$letterArray = str_split($word);
+		
 		// Alphabetize that array
 		sort($letterArray);
 
 		// For each entry in the letter array, output the letter.
 		foreach ($letterArray as $letter) {
+		
 			// Concatenate each letter onto $newString
 			$newString .= "$letter";
-		}
+		
+		} // end inner foreach
 
 		// After each word, add a space
 		$newString .= ' ';
-	}
+	} // end outer foreach
 
 	return $newString;
 
